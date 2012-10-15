2352e624ea080c4ed9d7013c1762e0ee7abf623d
Dan Schultz <slifty@gmail.com>
2012-10-15 19:15:22 -0400

    more rational default config

--- config.default.js
+++ config.default.js
@@ -1,12 +1,20 @@
 var constants = require('./constants'),
-var config = {}
+	config = {}
 
-config.stream = {}
+config.proxy = {};
+config.stream = {};
 config.web = {};
 
-config.stream.type = constants.STREAM_TYPE_SERIAL; // STREAM_TYPE_SERIAL || STREAM_TYPE_SOCKET
-config.stream.location = ""; // e.g. "/dev/tty.usbserial-FTBZ0DJP" or "http://localhost"
-config.stream.port = 0;
-config.web.port = process.env.WEB_PORT || 3000;
+config.stream.type = constants.STREAM_TYPE_SERVER; // enum(STREAM_TYPE_SERIAL, STREAM_TYPE_SERVER, STREAM_TYPE_CLIENT)
+config.stream.location = "http://openedcaptions.com";
+config.stream.port = 80; // Used for STREAM_TYPE_SERVER
+config.stream.secret = ""; // Used for STREAM_TYPE_CLIENT (a client is pushing the transcript to the server)
+
+config.proxy.mode = constants.PROXY_MODE_DISABLED; // enum(PROXY_MODE_ENABLED, PROXY_MODE_DISABLED);
+config.proxy.location = "localhost";
+config.proxy.port = 0;
+config.proxy.secret = ""; // This should be kept private
+
+config.web.port = 80;
 
 module.exports = config;
