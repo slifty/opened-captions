26d5bddf76179efce4b0bb7ec3c916ead90fc4c9
Dan Schultz <slifty@gmail.com>
2012-10-15 15:03:07 -0400

    removed some more unused express stuff

--- app.js
+++ app.js
@@ -19,14 +19,11 @@ var app = express(),
 	ioClient = require('socket.io-client')
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('port', config.web.port);
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
--- locales.js
+++ locales.js
@@ -8,7 +8,6 @@ if(typeof(window) != "undefined") {
 	if(!(LOCALE in localization))
 		localization[LOCALE] = localization[window.LOCALE_DEFAULT];
 } else {
-	// Todo: include every file in locales/ dynamically
 	exports.localization = {};
 	exports["default"] = require("./locales/default.js").localization["default"];
 }
