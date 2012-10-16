e5514ea4b0c1c639dab8e3e53ec8e5d2dd51a93f
Dan Schultz <slifty@gmail.com>
2012-10-16 14:45:31 -0400

    updated default config

--- config.default.js
+++ config.default.js
@@ -7,7 +7,7 @@ config.web = {};
 
 config.stream.type = constants.STREAM_TYPE_SERVER; // enum(STREAM_TYPE_SERIAL, STREAM_TYPE_SERVER, STREAM_TYPE_CLIENT)
 config.stream.location = "http://openedcaptions.com";
-config.stream.port = 80; // Used for STREAM_TYPE_SERVER
+config.stream.port = 3000; // Used for STREAM_TYPE_SERVER
 config.stream.secret = ""; // Used for STREAM_TYPE_CLIENT (a client is pushing the transcript to the server)
 
 config.proxy.mode = constants.PROXY_MODE_DISABLED; // enum(PROXY_MODE_ENABLED, PROXY_MODE_DISABLED);
@@ -15,6 +15,6 @@ config.proxy.location = "localhost";
 config.proxy.port = 0;
 config.proxy.secret = ""; // This should be kept private
 
-config.web.port = 80;
+config.web.port = 3000;
 
 module.exports = config;
