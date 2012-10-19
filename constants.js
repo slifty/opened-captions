29e1abed1a3eeca3d23a4e7b2b4e4a7b37b4cca3
Dan Schultz <slifty@gmail.com>
2012-10-19 13:07:51 -0400

    removed a few cluttering frills.  This is supposed to be bare bones.

--- constants.js
+++ constants.js
@@ -4,31 +4,24 @@ if(typeof(window) != "undefined") {
 }
 
 
-// Custom Payloads
+// Payloads
+exports.COMMUNICATION_GENERAL_PAYLOAD_ERROR = "error";
 exports.COMMUNICATION_TRANSCRIPT_PAYLOAD_CONTENT = "content";
 exports.COMMUNICATION_TRANSCRIPT_PAYLOAD_LINE = "line";
 exports.COMMUNICATION_TRANSCRIPT_PAYLOAD_WORD = "word";
-
-
-// General Payloads
-exports.COMMUNICATION_GENERAL_PAYLOAD_ERROR = "error";
-
-
-// General Constants
-exports.COMMUNICATION_SOCKET_BROADCAST = "broadcast";
-exports.COMMUNICATION_SOCKET_SERVER = "server";
-exports.LOCALE_DEFAULT = "default";
-
-exports.PROXY_MODE_DISABLED = "disabled";
-exports.PROXY_MODE_ENABLED = "enabled";
-
-exports.STREAM_TYPE_SERIAL = "serial";
-exports.STREAM_TYPE_SOCKET = "socket";
 
 
 // Targets
 exports.COMMUNICATION_TARGET_TRANSCRIPT = "transcript";
 
 
-// Viewports
-exports.VIEWPORT_TRANSCRIPT_CONTENTLIST = "transcript-contentlist";
+// Socket Types
+exports.COMMUNICATION_SOCKET_BROADCAST = "broadcast";
+exports.COMMUNICATION_SOCKET_SERVER = "server";
+
+
+// Configuration options
+exports.STREAM_TYPE_SERIAL = "serial";
+exports.STREAM_TYPE_SOCKET = "socket";
+exports.PROXY_MODE_DISABLED = "disabled";
+exports.PROXY_MODE_ENABLED = "enabled";
