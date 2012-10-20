d67cd1c0b7a8cbca0d96b17e9ad6c1691c7c4971
Dan Schultz <slifty@gmail.com>
2012-10-20 14:14:49 -0400

    fixed bug where constants and payload was breaking backbone

--- constants.js
+++ constants.js
@@ -1,28 +1,24 @@
-if(typeof(window) != "undefined") {
-	var exports = window;
-} else {
-}
+(function(exports) {
+	// Payloads
+	exports.COMMUNICATION_GENERAL_PAYLOAD_ERROR = "error";
+	exports.COMMUNICATION_TRANSCRIPT_PAYLOAD_CONTENT = "content";
+	exports.COMMUNICATION_TRANSCRIPT_PAYLOAD_LINE = "line";
+	exports.COMMUNICATION_TRANSCRIPT_PAYLOAD_WORD = "word";
 
 
-// Payloads
-exports.COMMUNICATION_GENERAL_PAYLOAD_ERROR = "error";
-exports.COMMUNICATION_TRANSCRIPT_PAYLOAD_CONTENT = "content";
-exports.COMMUNICATION_TRANSCRIPT_PAYLOAD_LINE = "line";
-exports.COMMUNICATION_TRANSCRIPT_PAYLOAD_WORD = "word";
+	// Targets
+	exports.COMMUNICATION_TARGET_TRANSCRIPT = "transcript";
 
 
-// Targets
-exports.COMMUNICATION_TARGET_TRANSCRIPT = "transcript";
+	// Socket Types
+	exports.COMMUNICATION_SOCKET_BROADCAST = "broadcast";
+	exports.COMMUNICATION_SOCKET_SERVER = "server";
 
 
-// Socket Types
-exports.COMMUNICATION_SOCKET_BROADCAST = "broadcast";
-exports.COMMUNICATION_SOCKET_SERVER = "server";
-
-
-// Configuration options
-exports.STREAM_TYPE_PROXY = "proxy";
-exports.STREAM_TYPE_SERIAL = "serial";
-exports.STREAM_TYPE_SERVER = "server";
-exports.PROXY_MODE_DISABLED = "disabled";
-exports.PROXY_MODE_ENABLED = "enabled";
+	// Configuration options
+	exports.STREAM_TYPE_PROXY = "proxy";
+	exports.STREAM_TYPE_SERIAL = "serial";
+	exports.STREAM_TYPE_SERVER = "server";
+	exports.PROXY_MODE_DISABLED = "disabled";
+	exports.PROXY_MODE_ENABLED = "enabled";
+}) (typeof(window) === "undefined"?exports:window);
--- payloads.js
+++ payloads.js
@@ -1,93 +1,90 @@
-if(typeof(window) != "undefined") {
-	var exports = window;
-	var constants = window;
-} else {
-	constants = require('./constants');
-}
-
-// Generic Payloads
-exports.ErrorPayload = function(content) {
-	this.content = content;
-	this.getPayload = function() {
-		return {
-			type: constants.COMMUNICATION_GENERAL_PAYLOAD_ERROR,
-			data: {
-				content: this.content
+(function(exports, constants) {
+	// Generic Payloads
+	exports.ErrorPayload = function(content) {
+		this.content = content;
+		this.getPayload = function() {
+			return {
+				type: constants.COMMUNICATION_GENERAL_PAYLOAD_ERROR,
+				data: {
+					content: this.content
+				}
 			}
-		}
+		};
 	};
-};
 
 
-// Module Payloads
-exports.TranscriptContentInPayload = function(body) {
-	this.body = body;
-	this.getPayload = function() {
-		return {
-			type: constants.COMMUNICATION_TRANSCRIPT_PAYLOAD_CONTENT,
-			data: {
-				body: this.body
+	// Module Payloads
+	exports.TranscriptContentInPayload = function(body) {
+		this.body = body;
+		this.getPayload = function() {
+			return {
+				type: constants.COMMUNICATION_TRANSCRIPT_PAYLOAD_CONTENT,
+				data: {
+					body: this.body
+				}
 			}
-		}
+		};
 	};
-};
 
-exports.TranscriptContentOutPayload = function(body) {
-	this.body = body;
-	this.getPayload = function() {
-		return {
-			type: constants.COMMUNICATION_TRANSCRIPT_PAYLOAD_CONTENT,
-			data: {
-				body: this.body
+	exports.TranscriptContentOutPayload = function(body) {
+		this.body = body;
+		this.getPayload = function() {
+			return {
+				type: constants.COMMUNICATION_TRANSCRIPT_PAYLOAD_CONTENT,
+				data: {
+					body: this.body
+				}
 			}
-		}
+		};
 	};
-};
 
-exports.TranscriptLineInPayload = function(body) {
-	this.body = body;
-	this.getPayload = function() {
-		return {
-			type: constants.COMMUNICATION_TRANSCRIPT_PAYLOAD_LINE,
-			data: {
-				body: this.body
+	exports.TranscriptLineInPayload = function(body) {
+		this.body = body;
+		this.getPayload = function() {
+			return {
+				type: constants.COMMUNICATION_TRANSCRIPT_PAYLOAD_LINE,
+				data: {
+					body: this.body
+				}
 			}
-		}
+		};
 	};
-};
 
-exports.TranscriptLineOutPayload = function(body) {
-	this.body = body;
-	this.getPayload = function() {
-		return {
-			type: constants.COMMUNICATION_TRANSCRIPT_PAYLOAD_LINE,
-			data: {
-				body: this.body
+	exports.TranscriptLineOutPayload = function(body) {
+		this.body = body;
+		this.getPayload = function() {
+			return {
+				type: constants.COMMUNICATION_TRANSCRIPT_PAYLOAD_LINE,
+				data: {
+					body: this.body
+				}
 			}
-		}
+		};
 	};
-};
 
-exports.TranscriptWordInPayload = function(body) {
-	this.body = body;
-	this.getPayload = function() {
-		return {
-			type: constants.COMMUNICATION_TRANSCRIPT_PAYLOAD_WORD,
-			data: {
-				body: this.body
+	exports.TranscriptWordInPayload = function(body) {
+		this.body = body;
+		this.getPayload = function() {
+			return {
+				type: constants.COMMUNICATION_TRANSCRIPT_PAYLOAD_WORD,
+				data: {
+					body: this.body
+				}
 			}
-		}
+		};
 	};
-};
 
-exports.TranscriptWordOutPayload = function(body) {
-	this.body = body;
-	this.getPayload = function() {
-		return {
-			type: constants.COMMUNICATION_TRANSCRIPT_PAYLOAD_WORD,
-			data: {
-				body: this.body
+	exports.TranscriptWordOutPayload = function(body) {
+		this.body = body;
+		this.getPayload = function() {
+			return {
+				type: constants.COMMUNICATION_TRANSCRIPT_PAYLOAD_WORD,
+				data: {
+					body: this.body
+				}
 			}
-		}
+		};
 	};
-};
+})(
+	typeof(window) === "undefined"?exports:window,
+	typeof(window) === "undefined"?require('./constants'):window);
