6023f213ea23edae818115654b109441e0755939
slifty <slifty@gmail.com>
2015-05-15 00:17:41 -0400

    introduction of messages and sockets

--- test/unit/random.stream.test.js
+++ test/unit/random.stream.test.js
@@ -8,7 +8,9 @@ describe('The random stream', function() {
   it('can be added to an opened captions instance', function(done) {
     var oc = new OpenedCaptions();
     oc.addStream('random');
-    oc.getStreams().length.should.equal(1);
+    Object.keys(oc.getStreams()).length.should.equal(1);
+    oc.close();
     done();
   });
+
 });
